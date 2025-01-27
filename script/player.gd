9cce8b7aea9ac1eec8d211418dc3f291524a4fe0
Palguna1121 <palguna2212@gmail.com>
2025-01-27 20:32:15 +0800

    initialize learn-2d-godot

---
+++ script/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 250.0
+const JUMP_VELOCITY = -450.0
+@onready var player_ui: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if (Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("ui_accept")) and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if velocity.y == JUMP_VELOCITY:
+			print("lompat")
+			player_ui.animation = "loncattt"	
+	# Handle Movement with arrow key / (a/d).
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		player_ui.flip_h = direction < 0
+		velocity.x = direction * SPEED
+		player_ui.animation = "run"
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		player_ui.animation = "idle"
+
+	# activate movement.
+	move_and_slide()
